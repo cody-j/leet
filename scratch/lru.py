d710fe9d886bcc794c74f570f46e643837df02b7
Cody J <codywj777@gmail.com>
2024-11-08 11:23:54 -0800

    basic insert and getters working;2C

--- scratch/lru.py
+++ scratch/lru.py
@@ -3,6 +3,8 @@ class Node:
         self.key, self.val = key, val
         self.next = None
         self.prev = None
+    def __hash__(self):
+        return hash(self.key)
 
 # doubly linked
 class LinkedHashMap:
@@ -12,26 +14,65 @@ class LinkedHashMap:
         self.tail = Node(None)
         self.head.next, self.tail.prev = self.tail, self.head
 
+    def add_to_tail(self, key, val):
+        if key in self.hash_map:
+            # cache hit
+            n = self.hash_map[key]
+            self._remove(n)
+            self._append(n)
+        else:
+            # cache miss
+            n = Node(key, val)
+            self.hash_map[key] = n
+            self._append(n)
+
+    def get(self, key):
+        if key in self.hash_map:
+            return self.hash_map[key].val
+        else:
+            return -1
+
+    def _append(self, node):
+        node.next, node.prev = self.tail, self.tail.prev
+        node.prev.next = node.next.prev = node
+
+    def _remove(self, node):
+        node.prev.next, node.next.prev = node.next, node.prev
+
     def __str__(self):
         return str(self.hash_map)
+
+    def print_follow(self):
+        n = self.head.next
+        while n.next:
+            print(n.key)
+            n = n.next
+
+    def __len__(self):
+        return len(self.hash_map)
 
 class LRU:
     def __init__(self, capacity=10):
         self.capacity = capacity
-        self.cache = LinkedHashMap()
+        self.items = LinkedHashMap()
 
-    def put(self):
-        pass
+    def put(self, key, val):
+        self.items.add_to_tail(key, val)
 
-    def get(self):
-        pass
+    def get(self, key):
+        return self.items.get(key)
 
     def __len__(self):
         return self.capacity
 
     def __repr__(self):
-        return str(self.cache)
+        return str(self.items)
 
 if __name__=="__main__":
     cache = LRU()
-    print(cache)
+    cache.put('foo', '1')
+    cache.put('bar', '2')
+    cache.items.print_follow()
+    cache.put('baz', '3')
+    cache.items.print_follow()
+    print(cache.get('asdf'))
