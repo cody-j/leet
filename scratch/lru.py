38c2d46b34e1a78f15823822a6a2d38c24d46bca
Cody J <codywj777@gmail.com>
2024-11-08 10:58:15 -0800

    initialize linked hash map

--- scratch/lru.py
+++ scratch/lru.py
@@ -1,8 +1,24 @@
+class Node:
+    def __init__(self, key, val=None):
+        self.key, self.val = key, val
+        self.next = None
+        self.prev = None
+
+# doubly linked
+class LinkedHashMap:
+    def __init__(self):
+        self.hash_map = {}
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def __str__(self):
+        return str(self.hash_map)
 
 class LRU:
     def __init__(self, capacity=10):
         self.capacity = capacity
-        # implement some linked list-based cache
+        self.cache = LinkedHashMap()
 
     def put(self):
         pass
@@ -10,13 +26,12 @@ class LRU:
     def get(self):
         pass
 
-    def __repr__(self):
-        return 'lru cache!'
-
     def __len__(self):
         return self.capacity
+
+    def __repr__(self):
+        return str(self.cache)
 
 if __name__=="__main__":
     cache = LRU()
     print(cache)
-    print(len(cache))
