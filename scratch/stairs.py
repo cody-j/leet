d5411eae9c28e66f3d658f5d3feb3f05cadeeacc
Cody J <codywj777@gmail.com>
2024-11-13 22:38:18 -0800

    refactors stair permutation climbing; previous and previous minus 1 summed until top

--- scratch/stairs.py
+++ scratch/stairs.py
@@ -1,5 +1,9 @@
 def climb_stairs(n):
-    pass
+    t = [0]*(n+1)
+    t[0] = t[1] = 1
+    for i in range(2, n+1):
+        t[i] = t[i-1]+ t[i-2]
+    return t[n]
 
 if __name__=="__main__":
-    climb_stairs(12)
+    print(climb_stairs(4))
