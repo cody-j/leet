a064d89e36a220e134e4156a55378b916066566c
Cody J <codywj777@gmail.com>
2024-11-18 20:21:45 -0800

    collect water from histogram passing hackerrank

--- scratch/list.py
+++ scratch/list.py
@@ -38,7 +38,41 @@ def justify(words, max_width=16):
     return justified
 
 
+# accumulative algorithm (total water captured by histogram-flavoured input)
+def trap(heights):
+    left_walls = []
+    b = 0 # calculated volume h
+    v = 0 # total volume
+    for i in range(1, len(heights)):
+        h = heights[i]
+        p = heights[i-1]
+        if h < p: # going down
+            left_walls.append((i-1,p)) # add previous as left wall
+            b=h
+        elif p < h: # going up
+            if not left_walls:
+                continue
+            while left_walls and b<min(h,left_walls[-1][1]):
+                li, lh = left_walls[-1] # values at top of the stack
+                th = min(h, lh)-b # top of the area to calulate (b is bottom)
+                tw = i-li-1 # width of area to calculate
+                v+=th*tw
+                b=min(h,lh)
+                # if min(h,lh) == lh:
+                if lh <= h:
+                    left_walls.pop()
+                # if min(h, lh) == h:
+    return v
+
+
+
+
+
+
 if __name__=="__main__":
-    justified = justify(["This", "is", "an", "example", "of", "text", "justification."])
-    for line in justified:
-        print(line)
+    # justified = justify(["This", "is", "an", "example", "of", "text", "justification."])
+    # for line in justified:
+    #     print(line)
+
+    trapped = trap([0,1,0,2,1,0,1,3,2,1,2,1])
+    print(trapped) # expected 6
